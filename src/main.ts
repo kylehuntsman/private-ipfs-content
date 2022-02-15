26678f8517bb38b8d594270097ccaebfeb14d22c
Laurent Senta <laurent@singulargarden.com>
2022-02-15 16:36:46 +0100

    hack: just testing out APIs, it works, I can kick out a node if they do not support my protocol

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ const MPLEX = require("libp2p-mplex");
 import Bootstrap from "libp2p-bootstrap";
 import { multiaddr } from "multiaddr";
 import process from "process";
+import pipe from "it-pipe";
+import { Connection } from "libp2p/src/connection-manager";
+// import {map} from 'streaming-iterables';
+// import {toBuffer} from 'it-buffer';
 
 // Known peers addresses
 const bootstrapMultiaddrs = [
@@ -38,8 +42,91 @@ const main = async () => {
     },
   });
 
+  const addr = process.argv[2] || "null";
+
+  const protocol = process.argv[3] || "none";
+
+  if (process.argv.length >= 4) {
+    console.log(`adding protocol ${protocol}`);
+
+    node.handle(`${protocol}`, ({ stream }) => {
+      pipe(stream, (source) =>
+        (async function () {
+          for await (const msg of source) {
+            console.log("message for protocol:", protocol, msg.toString());
+          }
+        })()
+      );
+    });
+  } else {
+    console.log("no custom protocol given, skipping.");
+  }
+
+  node.on("peer:discovery", (peer) => {
+    // console.log("Discovered %s", peer.id.toB58String()); // Log discovered peer
+    console.log(peer, peer.id);
+  });
+
+  node.connectionManager.on("peer:connect", async (connection: Connection) => {
+    console.log("Connected to %s", connection.remotePeer.toB58String()); // Log connected peer
+    // console.log(connection);
+    // console.log("protocol:", protocol);
+    // console.log("remote addr:", connection.remoteAddr);
+    // console.log("remote peer:", connection.remotePeer);
+
+    if (addr === "null") {
+      console.log("skip");
+      return;
+    }
+
+    try {
+      const { stream } = await node.dialProtocol(
+        connection.remotePeer,
+        protocol
+      );
+      // const { stream } = await node.dialProtocol(connection.remoteAddr, protocol);
+      await pipe(["helloworld from custom protocol", stream]);
+      console.log(
+        "nice, it worked",
+        connection.remotePeer.toB58String(),
+        "supports our protocol"
+      );
+    } catch (err) {
+      // @ts-ignore
+      if (err.code === "ERR_UNSUPPORTED_PROTOCOL") {
+        console.error("OK not supported, close this connection");
+        await connection.close();
+      } else {
+        console.error(err);
+      }
+    }
+
+    // connection.addStream(stream, {protocol})
+    // const {stream} = await node.dialProtocol(connection.remotePeer, [protocol])
+    // await pipe(
+    //     ['another stream on protocol (b)'],
+    //     stream3
+    //   )
+
+    //   connection.addStream(stream, {protocol})
+
+    // Do one thing right now, allow a connection IFF you speak my protocol.
+  });
+
   await node.start();
   console.log("libp2p has started");
+
+  // ping peer if received multiaddr
+  if (process.argv.length >= 3) {
+    if (addr !== "null") {
+      const ma = multiaddr(process.argv[2]);
+      console.log(`pinging remote peer at ${process.argv[2]}`);
+      const latency = await node.ping(ma);
+      console.log(`pinged ${process.argv[2]} in ${latency}ms`);
+    }
+  } else {
+    console.log("no remote peer address given, skipping ping");
+  }
 
   // print out listening addresses
   console.log("listening on addresses:");
@@ -47,29 +134,14 @@ const main = async () => {
     console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`);
   });
 
-  // ping peer if received multiaddr
-  if (process.argv.length >= 3) {
-    const ma = multiaddr(process.argv[2]);
-    console.log(`pinging remote peer at ${process.argv[2]}`);
-    const latency = await node.ping(ma);
-    console.log(`pinged ${process.argv[2]} in ${latency}ms`);
-  } else {
-    console.log("no remote peer address given, skipping ping");
-  }
+  await sleep(3000);
+  console.log(node.connections);
 
-  node.on("peer:discovery", (peer) => {
-    console.log("Discovered %s", peer.id.toB58String()); // Log discovered peer
-  });
+  //   await sleep(100000);
 
-  node.connectionManager.on("peer:connect", (connection) => {
-    console.log("Connected to %s", connection.remotePeer.toB58String()); // Log connected peer
-  });
-
-//   await sleep(100000);
-
-//   // stop libp2p
-//   await node.stop();
-//   console.log("libp2p has stopped");
+  //   // stop libp2p
+  //   await node.stop();
+  //   console.log("libp2p has stopped");
 
   const stop = async () => {
     // stop libp2p
